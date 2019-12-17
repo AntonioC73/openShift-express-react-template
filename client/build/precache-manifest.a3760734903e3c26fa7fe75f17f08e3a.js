32f8f6f99c9efea3b35540a47597a9d0bd5c97d7
antonioc <antoniocastellucci@it.ibm.com>
2019-12-17 21:58:03 +0100

    react-leaflet integrated

---
+++ client/build/precache-manifest.a3760734903e3c26fa7fe75f17f08e3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb1763379ec1601dc70733427c7448e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a958886c10848d68c9f",
+    "url": "/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "ceacd3a50adea1db6481",
+    "url": "/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "0a958886c10848d68c9f",
+    "url": "/static/js/2.a9ef6344.chunk.js"
+  },
+  {
+    "revision": "fd64d766fb7e6d1a18e038d229e55314",
+    "url": "/static/js/2.a9ef6344.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ceacd3a50adea1db6481",
+    "url": "/static/js/main.00034d9e.chunk.js"
+  },
+  {
+    "revision": "d55ef5193bcbc1fa7b06",
+    "url": "/static/js/runtime-main.5bd98d82.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,161 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'antd/es/button';
 import './App.css';
+import { Table, Divider, Tag } from 'antd';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      sampleData: {}
-    };
-  }
-
-  componentDidMount() {
-    fetch('/sample_data')
-      .then(res => res.json())
-      .then(sampleData => this.setState({ sampleData }))
-      .catch(error => console.log(error));
-  }
-
-  renderSampleDataEntry() {
-    return Object.keys(this.state.sampleData).map(key => (
-      <tr key={key}>
-        <td>{key}</td>
-        <td>{this.state.sampleData[key]}</td>
-      </tr>
-    ));
-  }
-
-  renderSampleData() {
-    return (
-      <table className="sample-data-table">
-        <thead>
-          <tr>
-            <th>Keys</th>
-            <th>Values</th>
-          </tr>
-        </thead>
-        <tbody>
-        { this.renderSampleDataEntry() }
-        </tbody>
-      </table>
-    )
-  }
-
+	
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>Sample Data returned from Express: /sample_data endpoint</h3>
-        <div className="sample-data-container">
-          { this.renderSampleData() }
-        </div>
+	  
+	const position = [this.state.lat, this.state.lng]
+	
+	const markers = (<React.Fragment>
+	{this.state.data.map((d) => {
+		d.position = position.map((x)=>{ return x+Math.random()/100});
+		return (<Marker position={d.position}><Popup>{d.checkpoint}: {d.count}</Popup></Marker>);
+	})}
+	</React.Fragment>);
+	
+    return (      
+      <div>
+	     
+	      <Map center={position} zoom={this.state.zoom}>
+	      <TileLayer
+	        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+	      {markers}
+	      </Map>
+	      
+	     
+	     <Table columns={columns} dataSource={this.state.data} bordered="true" pagination={{pageSize: 20}} showHeader="true" size="small" />
+	     {/* todo */}
       </div>
+
     );
   }
+  
+  
+  
+  componentDidMount() {	  
+	  //this.intervalId = setInterval(() => this.loadData(), 60000);
+	  this.loadData(); // also load one immediately
+  }  
+  
+  loadData(){
+	  fetch('http://ac-node-nr-consumer-ac-project.192.168.202.24.nip.io/api/v2/status',
+	    {
+	        method: 'GET',
+	        //body: JSON.stringify(body), //eventuale json in body
+	        headers: {
+	            "Content-Type": "application/json; charset=utf-8",
+				"Authorization": "Bearer 1234567890",
+				"Accept":"application/json"
+			},
+	    })
+	    .then(response => response.json()).then((json) => {
+	      this.setState({ data: json });
+	    }).catch((error) => alert(error.message));
+	  
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  
+  state = {
+		  lat: 40.7462,
+		  lng: 14.4989,
+		  zoom: 17,
+		  data: [
+		    {
+		      key: '1',
+		      checkpoint: 'PortaMarina',		      
+		      checkpointtype: 'crossing',
+		      direction: 'in',
+			  count: 12,
+		      tags: ['nice', 'developer'],
+		    },
+		    {
+		      key: '2',
+		      checkpoint: 'Anfiteatro',
+		      checkpointtype: 'crossing',
+		      direction: 'out',
+			  count: 8,
+		      tags: ['loser'],
+		    },
+		    {
+		      key: '3',
+		      checkpoint: 'Foro',
+		      checkpointtype: 'crossing',
+		      direction: 'in',
+			  count: 10,
+		      tags: ['cool', 'teacher'],
+		    },
+		  ]
+  };
+  
+  
 }
 
+const columns = [
+  {
+    title: 'Checkpoint',
+    dataIndex: 'checkpoint',
+    key: 'checkpoint',
+	width: 150,
+	sorter: (a, b) => a.checkpoint.length - b.checkpoint.length,
+	sortDirections: ['ascend']	    
+  },
+  {
+    title: 'CheckpointType',
+    dataIndex: 'checkpointtype',
+    key: 'checkpointtype',
+	width: 200,
+	  filters: [
+	        {
+	          text: 'crossing',
+	          value: 'crossing',
+	        },
+	        {
+	          text: 'baggage-drop',
+	          value: 'baggage-drop',
+	        }
+	      ],
+	      // specify the condition of filtering result
+	      // here is that finding the name started with `value`
+	      onFilter: (value, record) => record.checkpointtype.indexOf(value) === 0,
+	      sorter: (a, b) => a.checkpointtype.length - b.checkpointtype.length,
+	      sortDirections: ['ascend'],
+  },
+  {
+    title: 'Direction',
+    dataIndex: 'direction',
+    key: 'direction',
+	  width: 80
+  },
+  {
+    title: 'Count',
+    dataIndex: 'count',
+    key: 'count',
+	  width: 80 ,
+    render: text => <b>{text}</b>
+  },
+  {
+    title: 'Time',
+    dataIndex: 'updatetime',
+    key: 'updatetime',
+	   width: 200 ,
+  },
+  {
+    title: 'Other',
+    dataIndex: 'info',
+    key: 'info'
+  }
+  
+]
+
 export default App;
+
+//ReactDOM.render(<Table columns={columns} dataSource={data} />, mountNode);
